cacd5f39937b218be49af5821ec245fb4e2de809
Casper Korsgaard <cko@charlietango.dk>
2020-02-12 12:46:13 +0100

    Update HilbertCurve.ts

--- src/HilbertCurve.ts
+++ src/HilbertCurve.ts
@@ -1,27 +1,13 @@
 class HilbertCurve {
 
   static rotate(n: number, point: number[], rotateX: number, rotateY: number) {
-
-    if (rotateY !== 0) return point;
-      
-    if (rotateX === 1) point = [(n - 1) - point[0], (n - 1) - point[1]];
-
-    // point[0] = (n - 1) - point[0];
-    // point[1] = (n - 1) - point[1];
-    
+    if (rotateY !== 0) return point;      
+    if (rotateX === 1) point = [(n - 1) - point[0], (n - 1) - point[1]];    
     point = [point[1], point[0]];
-    // point = [point[1], t];
-    // point[0] = point[1];
-    // point[1] = t;
-    
 		return point;
   }  
 
-  static numberBoolean(b: boolean): number {
-    return b ? 1 : 0;
-  } 
-
-  // todo: refacor
+  // todo: make strong
   public static pointToDistance(point: number[], order: number): number {
     let 
       rotateX, // number | boolean
@@ -33,7 +19,6 @@ class HilbertCurve {
       rotateX = (point[0] & s) > 0;
       rotateY = (point[1] & s) > 0;
 
-
       distance += s * s * ((3 * rotateX) ^ rotateY);
 
       this.rotate(s, point, rotateX, rotateY);
@@ -44,8 +29,8 @@ class HilbertCurve {
 
   public static distanceToPoint(order: number, distance: number): Array<number> {
     let 
-      rotateX: number, // for power of 
-      rotateY: number,  // for power of 
+      rotateX: number, 
+      rotateY: number,  
       s: number, 
       t: number, 
       x: number, 
@@ -56,19 +41,13 @@ class HilbertCurve {
     x = y = 0;
   
     for (s = 1; s < order; s *= 2) {      
-
       rotateX = 1 & (t / 2);
 			rotateY = 1 & (t ^ rotateX);
 
       let point = this.rotate(s, [x, y], rotateX, rotateY);
 
       [x, y] = [point[0], point[1]]; 
-      // x = point[0];
-			// y = point[1];
-
-      [x, y] = [x + s * rotateX, y + s * rotateY]
-			// x = x + s * rotateX;
-			// y = y + s * rotateY;
+      [x, y] = [x + s * rotateX, y + s * rotateY];
 
       t /= 4;
     }
@@ -76,5 +55,6 @@ class HilbertCurve {
   }
   
 }
+
 export { HilbertCurve }
 export default { HilbertCurve }
